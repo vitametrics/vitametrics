2b3f3740cbc968e55a9d8c0db0663b7c294baa2a
Sean Cornell <seancornell11@gmail.com>
2024-01-26 09:51:12 -0800

    Only add CORS if NODE_ENV is set to production

--- backend/middleware/common.ts
+++ backend/middleware/common.ts
@@ -5,9 +5,11 @@ import cookieParser from 'cookie-parser';
 export const commonMiddlewares = (app: express.Application) => {
   app.use(express.json());
   app.use(cookieParser());
-  app.use(cors({
-    origin: process.env.BASE_URL as string,
-    credentials: true
-  }));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(cors({
+      origin: process.env.BASE_URL as string,
+      credentials: true
+    }));
+  }
   app.set('trust proxy', 1);
 };
