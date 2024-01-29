f372ad920a1061140b0c31166f3db5d1cf10da4e
Sean <seancornell11@gmail.com>
2024-01-28 22:38:48 -0800

    Add server-side sanitation for contact params

--- backend/routes/Contact.ts
+++ backend/routes/Contact.ts
@@ -1,17 +1,18 @@
 import express, {Request, Response} from 'express';
-//import rateLimit from 'express-rate-limit';
+import rateLimit from 'express-rate-limit';
 import sgMail from '@sendgrid/mail';
+import validator from 'validator';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 const router = express.Router();
 
-// const contactLimiter = rateLimit({
-//     windowMs: 15 * 60 * 1000, // 15 mins
-//     max: 10 // limit each IP to 10 requests per windowMs
-//   });
+const contactRateLimit= rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 10 // limit each IP to 10 requests per windowMs
+  });
 
-router.post('/', async(req: Request, res: Response) => {
+router.post('/', contactRateLimit, async(req: Request, res: Response) => {
     const {email, organizationName, msgBody} = req.body;
 
     console.log('email: ', email, '\norgName: ', organizationName, '\nmsgBody: ', msgBody);
@@ -20,11 +21,24 @@ router.post('/', async(req: Request, res: Response) => {
         return res.status(400).json({ msg: 'Please enter all fields' });
     }
 
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ msg: 'Invalid email format' }); 
+    }
+
+    // arbitrary length check
+    if (organizationName.length > 100 || msgBody.length > 1000) {
+        return res.status(400).json({ msg: 'Invalid input length' });
+    }
+
+    const sanitizedEmail = validator.escape(email);
+    const sanitizedOrgName = validator.escape(organizationName);
+    const sanitizedMsgBody = validator.escape(msgBody);
+
     const msg = {
         to: process.env.ADMIN_EMAIL as string,
         from: process.env.SENDGRID_FROM as string,
-        subject: `New Contact Message from ${organizationName}`,
-        text: `Message from ${email}: ${msgBody}`,
+        subject: `New Contact Message from ${sanitizedOrgName}`,
+        text: `Message from ${sanitizedEmail}: ${sanitizedMsgBody}`,
     };
 
     try {
