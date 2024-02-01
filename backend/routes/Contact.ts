eb11f50c527add524ccdf079096df0241745247e
Sean Cornell <seancornell11@gmail.com>
2024-02-01 10:27:59 -0800

    Add helmet and express-validator
    Added express-validator for param/body checks and helmet for header management.

--- backend/routes/Contact.ts
+++ backend/routes/Contact.ts
@@ -1,9 +1,7 @@
 import express, {Request, Response} from 'express';
 import rateLimit from 'express-rate-limit';
-import sgMail from '@sendgrid/mail';
-import validator from 'validator';
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+import {body, validationResult } from 'express-validator';
+import { sendEmail } from '../util/emailUtil';
 
 const router = express.Router();
 
@@ -12,38 +10,27 @@ const contactRateLimit= rateLimit({
     max: 10 // limit each IP to 10 requests per windowMs
   });
 
-router.post('/', contactRateLimit, async(req: Request, res: Response) => {
+router.post('/', contactRateLimit, [
+    body('email').isEmail().withMessage('Invalid email format'),
+    body('organizationName').isLength({max: 100}).withMessage('Organization name is too long'),
+    body('msgBody').isLength({max: 1000}).withMessage('Message body is too long')
+
+], async(req: Request, res: Response) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
     const {email, organizationName, msgBody} = req.body;
 
-    console.log('email: ', email, '\norgName: ', organizationName, '\nmsgBody: ', msgBody);
-
-    if (!email || !organizationName || !msgBody) {
-        return res.status(400).json({ msg: 'Please enter all fields' });
-    }
-
-    if (!validator.isEmail(email)) {
-        return res.status(400).json({ msg: 'Invalid email format' }); 
-    }
-
-    // arbitrary length check
-    if (organizationName.length > 100 || msgBody.length > 1000) {
-        return res.status(400).json({ msg: 'Invalid input length' });
-    }
-
-    const sanitizedEmail = validator.escape(email);
-    const sanitizedOrgName = validator.escape(organizationName);
-
-    const msg = {
-        to: process.env.ADMIN_EMAIL as string,
-        from: process.env.SENDGRID_FROM as string,
-        subject: `New Contact Message from ${sanitizedOrgName}`,
-        /*text: `Message from ${sanitizedEmail}: ${msgBody}`,*/
-        text: `Message from ${sanitizedEmail}: \n`,
-        html: msgBody, //test this
-    };
-
     try {
-        await sgMail.send(msg);
+        await sendEmail({
+            to: process.env.ADMIN_EMAIL as string,
+            subject: `New Contact Message from ${organizationName}`,
+            text: `Message from ${email}: \n`,
+            html: msgBody, //test this
+        })
         return res.status(200).json({ msg: 'Message sent successfully' });
     } catch (err) {
         console.error(err);
