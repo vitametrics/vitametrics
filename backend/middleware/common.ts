b7f1500dc06c1f01536b08d6d53a487e898674f0
Sean <seancornell11@gmail.com>
2024-02-16 14:12:15 -0800

    Fix session storage

--- backend/middleware/common.ts
+++ backend/middleware/common.ts
@@ -2,10 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import RedisStore from 'connect-redis';
-import {createClient} from 'redis';
+import MongoStore from 'connect-mongo';
 
-export const commonMiddlewares = (app: express.Application) => {
+export const commonMiddlewares = async (app: express.Application) => {
   app.use(express.json());
   app.use(cookieParser());
   if (process.env.NODE_ENV === 'production') {
@@ -15,22 +14,14 @@ export const commonMiddlewares = (app: express.Application) => {
     }));
   }
 
-  const redisClient = createClient();
-  redisClient.connect().catch(console.error);
-
-  const redisStore = new RedisStore({
-    client: redisClient,
-    prefix: "physiobit:",
-  });
-
   app.use(session({
     secret: process.env.SESSION_SECRET as string, 
     resave: false,
     saveUninitialized: false,
-    store: redisStore,
+    store: MongoStore.create({ mongoUrl: process.env.PROD_DB_URI as string }) as any,
     cookie: {
-      secure: false,
-      sameSite: false,
+      secure: true,
+      sameSite: 'lax',
       httpOnly: true,
     }
   }));
