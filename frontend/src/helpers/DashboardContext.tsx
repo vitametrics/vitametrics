1446ff8e87eaa14977ca200d221b8d9d236f7ea7
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-04-17 01:01:58 -0700

    fetch-data -- need to test

--- frontend/src/helpers/DashboardContext.tsx
+++ frontend/src/helpers/DashboardContext.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   useState,
   useContext,
   createContext,
   Dispatch,
   SetStateAction,
+  useEffect,
+  useRef,
 } from "react";
+import axios from "axios";
 
 import { useOrg } from "./OrgContext";
 
@@ -20,6 +24,14 @@ interface DashboardProps {
   selectedDevices: string[];
   setSelectedDevices: (arg0: string[]) => void;
   handleDeviceSelectionChange: (deviceId: string, isChecked: boolean) => void;
+  devicesData: any[]; //temp any
+}
+
+interface DeviceContext {
+  [deviceId: string]: {
+    rangeStartDate: Date;
+    rangeEndDate: Date;
+  };
 }
 
 const DashboardContext = createContext<DashboardProps | undefined>(undefined);
@@ -27,6 +39,11 @@ const DashboardContext = createContext<DashboardProps | undefined>(undefined);
 const DashboardProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const FETCH_DEVICE_DATA_ENDPOINT =
+    import.meta.env.NODE_ENV === "production"
+      ? import.meta.env.VITE_APP_FETCH_DEVICE_DATA_ENDPOINT
+      : import.meta.env.VITE_APP_FETCH_DEVICE_DATA_DEV_ENDPOINT;
+
   const { devices } = useOrg();
   const [startDate, setStartDate] = useState(new Date()); // use `new Date()` instead of `Date.now()`
   const [rangeStartDate, setRangeStartDate] = useState(new Date());
@@ -35,6 +52,18 @@ const DashboardProvider: React.FC<{ children: React.ReactNode }> = ({
   const [selectedDevices, setSelectedDevices] = useState<string[]>(
     devices.map((device) => device.id)
   );
+  const [devicesData, setDevicesData] = useState<any[]>([]); //temp any
+  const loadedDevicesRef = useRef<DeviceContext>({});
+
+  const formatDate = (date: Date) => {
+    const month =
+      date.getMonth() + 1 < 10
+        ? "0" + (date.getMonth() + 1)
+        : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+  };
 
   const handleDeviceSelectionChange = (
     deviceId: string,
@@ -46,6 +75,49 @@ const DashboardProvider: React.FC<{ children: React.ReactNode }> = ({
         : prev.filter((id) => id !== deviceId);
     });
   };
+
+  const shouldFetchDeviceData = (deviceId: string) => {
+    const loadedData = loadedDevicesRef.current[deviceId];
+    return (
+      !loadedData ||
+      loadedData.rangeStartDate !== rangeStartDate ||
+      loadedData.rangeEndDate !== rangeEndDate
+    );
+  };
+
+  const fetchDevice = (deviceId: string) => {
+    if (shouldFetchDeviceData(deviceId)) {
+      try {
+        axios
+          .get(FETCH_DEVICE_DATA_ENDPOINT, {
+            params: {
+              id: deviceId,
+              startDate: formatDate(rangeStartDate),
+              endDate: formatDate(rangeEndDate),
+            },
+          })
+          .then((res) => {
+            console.log(res.data);
+            setDevicesData((prev) => [...prev, res.data]);
+            loadedDevicesRef.current[deviceId] = {
+              rangeStartDate,
+              rangeEndDate,
+            };
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (selectedDevices.length > 0) {
+      setDevicesData([]);
+      selectedDevices.forEach((deviceId) => {
+        fetchDevice(deviceId);
+      });
+    }
+  }, [rangeStartDate, rangeEndDate, selectedDevices]);
 
   return (
     <DashboardContext.Provider
@@ -61,6 +133,7 @@ const DashboardProvider: React.FC<{ children: React.ReactNode }> = ({
         selectedDevices,
         setSelectedDevices,
         handleDeviceSelectionChange,
+        devicesData,
       }}
     >
       {children}
