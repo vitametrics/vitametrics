c848974dfdf20ef892472cb7d0ae95d92ae3f7f8
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-04-20 01:20:27 -0700

    styles optimization

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -34,30 +34,13 @@ export default {
         desc: "#868686",
       },
     },
-
-    /*
-    screens: {
-      sm: { min: "0", max: "767px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
-
-      md: { min: "768px" },
-      // => @media (min-width: 768px and max-width: 1023px) { ... }
-
-      lg: { min: "768px", max: "1279px" },
-      // => @media (min-width: 1024px and max-width: 1279px) { ... }
-
-      xl: { min: "1280px", max: "1535px" },
-      // => @media (min-width: 1280px and max-width: 1535px) { ... }
-
-      "2xl": { min: "1536px" },
-      // => @media (min-width: 1536px) { ... }
-    },*/
     fontFamily: {
       raleway: ["Raleway-Regular", "sans-serif"],
       ralewayBold: ["Raleway-Bold", "sans-serif"],
       ralewaySemibold: ["Raleway-Semibold", "sans-serif"],
       leagueSpartanBold: ["LeagueSpartan-Bold", "sans-serif"],
       leagueSpartan: ["LeagueSpartan-Regular", "sans-serif"],
+      leagueSpartanLight: ["LeagueSpartan-Light", "sans-serif"],
     },
   },
   plugins: ["@tailwindcss/forms"],
