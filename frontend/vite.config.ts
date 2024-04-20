cf85a10a9b26df815440d2fb58fe266f76fb1508
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-04-20 01:19:59 -0700

    lossless compression for optimization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),viteCompression({
+    algorithm: 'brotliCompress',
+    filter: (assetFileName) => assetFileName.endsWith('.js') || assetFileName.endsWith('.css'),
+  })],
 })
