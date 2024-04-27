9177bafd633fb0fe24bd44c90d007e41fedbc0d1
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-04-26 19:01:11 -0700

    svg syntax fix

--- frontend/src/assets/WarningIcon.tsx
+++ frontend/src/assets/WarningIcon.tsx
@@ -10,11 +10,11 @@ export const WarningIcon = () => {
       viewBox="0 0 478.125 478.125"
       stroke="#ffdd00"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
--- frontend/src/components/DashboardNavbar.tsx
+++ frontend/src/components/DashboardNavbar.tsx
@@ -2,9 +2,7 @@ import { useAuth } from "../helpers/AuthContext";
 import logo from "../assets/logo.webp";
 
 export const DashboardNavbar = () => {
-  //const { isAuthenticated } = useAuth();
   const { logout } = useAuth();
-
   return (
     <nav className="bg-[#161616] px-2">
       <div className="flex flex-row w-full bg-[#161616] top-0 flex-wrap items-center justify-between mx-auto py-3">
@@ -13,7 +11,7 @@ export const DashboardNavbar = () => {
           className="flex items-center space-x-3 rtl:space-x-reverse mr-auto"
         >
           <img src={logo} className="h-[2rem]" alt="Physiobit Logo" />
-        </a>{" "}
+        </a>
         <div className="flex items-center space-x-3">
           <a
             href="/"
@@ -27,35 +25,35 @@ export const DashboardNavbar = () => {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
               <g
                 id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></g>
               <g id="SVGRepo_iconCarrier">
                 {" "}
                 <path
                   d="M21 12L13 12"
                   stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>{" "}
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
                 <path
                   d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9"
                   stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>{" "}
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
                 <path
                   d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19"
                   stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>{" "}
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
               </g>
             </svg>
           </a>
