160c6b1419030b5a644243e4b3cbbca494c4faa6
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-05-03 20:55:04 -0700

    set password styling

--- frontend/src/pages/SetPassword.tsx
+++ frontend/src/pages/SetPassword.tsx
@@ -1,23 +1,54 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import axios from "axios";
 import { useAuth } from "../helpers/AuthContext";
+import logo from "../assets/images/vitamix.webp";
 
 const SetPassword = () => {
   const [searchParams] = useSearchParams({
     token: "",
   });
+
+  const navigate = useNavigate();
+
   const { login_from_set_password } = useAuth();
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [debouncedPassword, setDebouncedPassword] = useState("");
   const [debouncedConfirmPassword, setDebouncedConfirmPassword] = useState("");
   const token = searchParams.get("token");
+  const [isTokenValid, setIsTokenValid] = useState(false);
   const SET_PASSWORD_ENDPOINT =
     import.meta.env.VITE_APP_NODE_ENV === "production"
       ? import.meta.env.VITE_APP_SET_PASSWORD_ENDPOINT
       : import.meta.env.VITE_APP_SET_PASSWORD_DEV_ENDPOINT;
+
+  const CHECK_PASSWORD_TOKEN_ENDPOINT =
+    import.meta.env.VITE_APP_NODE_ENV === "production"
+      ? import.meta.env.VITE_APP_CHECK_PASSWORD_TOKEN_ENDPOINT
+      : import.meta.env.VITE_APP_CHECK_PASSWORD_TOKEN_DEV_ENDPOINT;
+
+  const check_password = async () => {
+    try {
+      const response = await axios.post(CHECK_PASSWORD_TOKEN_ENDPOINT, {
+        token: searchParams.get("token"),
+      });
+
+      if (response.data) {
+        setIsTokenValid(true);
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+      setIsTokenValid(false);
+    }
+  };
+
+  useEffect(() => {
+    check_password();
+  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -65,25 +96,61 @@ const SetPassword = () => {
   };
 
   return (
-    <div className="bg-fixed w-full h-full">
+    <div className="h-full w-full bg-fixed overflow-y-hidden font-leagueSpartanBold ">
       <Navbar />
-      {token ? (
-        <div className="bg-glass">
-          <input
-            type="password"
-            placeholder="Enter new password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Confirm new password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <button onClick={handleSubmit}>Submit</button>
+      <div className="flex flex-col justify-center items-center p-0 md:p-10">
+        <div className="flex flex-row h-screen items-center justify-center">
+          {isTokenValid ? (
+            <div className="flex flex-col pt-32 items-center justify-center bg-glass w-full h-full md:w-[500px] md:h-[600px]  p-20 md:pt-20 rounded-none md:rounded-xl ">
+              <a href="/" className="mb-5">
+                <img
+                  src={logo}
+                  onClick={() => navigate("/")}
+                  className="h-20 rounded-xl border-primary"
+                  alt="VitametricsLogo"
+                />
+              </a>
+              <h2 className="font-bold text-4xl w-72 mt-5 text-center text-primary">
+                {" "}
+                Set Password{" "}
+              </h2>
+              <input
+                className="p-[10px] mt-5 w-72 bg-[#d2d1d1] text-black  rounded-lg border-[#6d6c6c]"
+                type="password"
+                placeholder="Enter new password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                className="p-[10px] mt-5 w-72 bg-[#d2d1d1] text-black  rounded-lg border-[#6d6c6c]"
+                type="password"
+                placeholder="Confirm new password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <button
+                onClick={handleSubmit}
+                className="p-[10px] mt-5 bg-secondary w-72 rounded-lg cursor-pointer font-bold text-white"
+              >
+                {" "}
+                Login{" "}
+              </button>
+            </div>
+          ) : (
+            <div className="flex flex-col pt-32 items-center justify-center bg-glass w-full h-full md:w-[500px] md:h-[600px]  p-20 md:pt-20 rounded-none md:rounded-xl text-2xl">
+              <a href="/" className="mb-32">
+                <img
+                  src={logo}
+                  onClick={() => navigate("/")}
+                  className="h-20 rounded-xl border-primary"
+                  alt="VitametricsLogo"
+                />
+              </a>
+
+              <a className="mb-auto">Invalid token</a>
+            </div>
+          )}
         </div>
-      ) : (
-        <div>Invalid token</div>
-      )}
+      </div>
+      <Footer />
     </div>
   );
 };
