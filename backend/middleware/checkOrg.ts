3d5b876a6a510cfa896b39b60aee64cea596216c
Sean <seancornell11@gmail.com>
2024-05-05 14:24:53 -0700

    [feat] modify middleware and models for project-based structure

--- backend/middleware/checkOrg.ts
+++ backend/middleware/checkOrg.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import Organization from '../models/Organization';
+import Organization, { IOrganization } from '../models/Organization';
 import User from '../models/User';
 import { CustomReq } from '../types/custom';
 
@@ -15,21 +15,32 @@ const checkOrgMembership = async (expressReq: Request, res: Response, next: Next
 
     try {
 
-        const user = await User.findOne({userId: userId});
+        const user = await User.findOne({ userId: userId}).populate('organizations');
 
         if (!user) {
             return res.status(404).json({msg: 'User not found'});
         }
 
-        const organization = await Organization.findOne({ orgId: user.orgId});
+        if (!user.organizations.length && user.role !== 'owner') {
+            return res.status(403).json({ msg: 'Access denied - User not a member of any organization' });
+        }
 
-        if (!organization) {
-            return res.status(403).json({ msg: 'Organization not found in middleware' });
-        } else if (!organization.members.some(memberId => memberId.equals(user._id))) {
-            return res.status(403).json({ msg: 'Access denied - User not a member of any organization', data: `user._id: ${user._id}` });
-	    }
+        const orgId = req.body.projectId || req.query.projectId;
+        const organizationIds = user.organizations.map(org => org._id);
+        console.log(organizationIds);
+        console.log(orgId);
+        const matchingOrg = await Organization.findOne({
+            orgId: orgId,
+            _id: { $in: organizationIds }
+        });
 
-        req.organization = organization;
+        if (!matchingOrg && user.role !== 'owner') {
+            return res.status(403).json({ msg: 'Access denied - User not a member of the organization' });
+        }
+
+        req.organization = matchingOrg as IOrganization;
+
+        console.log('was in org');
         return next();
         
     } catch (err) {
