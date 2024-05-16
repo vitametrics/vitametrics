e4569bef16032b56faa85eea4a2fba7085bfcce8
Sean <seancornell11@gmail.com>
2024-05-15 22:40:45 -0700

    [bugfix] more verify session testing

--- backend/middleware/verifySession.ts
+++ backend/middleware/verifySession.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 import HandleResponse from '../types/response';
 
 const verifySession = (req: Request, res: Response, next: NextFunction) => {
+    console.log('Session ID:', req.sessionID);
+    console.log('User:', req.user);
+
     if (req.isAuthenticated && req.isAuthenticated()) {
         console.log('User is authenticated');
         return next();
@@ -10,5 +13,4 @@ const verifySession = (req: Request, res: Response, next: NextFunction) => {
     console.log('User is not authenticated');
     res.status(401).json(new HandleResponse("Unauthorized: Please log in.", 401));
 };
-
 export default verifySession;
