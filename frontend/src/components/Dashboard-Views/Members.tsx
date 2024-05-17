9738be6fa86a7e2175ff805b976422fde1ff17e4
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-05-16 21:05:11 -0700

    [chore] - Renewed Menbers

--- frontend/src/components/Dashboard-Views/Members.tsx
+++ frontend/src/components/Dashboard-Views/Members.tsx
@@ -1,7 +1,7 @@
-import { useOrg } from "../../helpers/OrgContext";
+import { useProject } from "../../helpers/ProjectContext";
+import { useDashboard } from "../../helpers/DashboardContext";
 import { useAuth } from "../../helpers/AuthContext";
 import { useSearchParams } from "react-router-dom";
-import { useDashboard } from "../../helpers/DashboardContext";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import useDebounce from "../../helpers/useDebounce";
@@ -25,8 +25,9 @@ const Members = () => {
     threshold: 0.1, // Adjust based on when you want the animation to trigger (1 = fully visible)
     triggerOnce: true, // Ensures the animation only plays once
   });
-  const { orgName, members, fetchOrg } = useOrg();
-  const { isOrgOwner, userId } = useAuth();
+  const { projectName, members, fetchProject } = useProject();
+  const { isOwner } = useDashboard();
+  const { userId } = useAuth();
   const [searchParams, setSearchParams] = useSearchParams({
     view: "members",
     showInviteMenu: "false",
@@ -34,12 +35,15 @@ const Members = () => {
     name: "",
     member: "",
     invited: "false",
+    role: "user",
   });
   const showInviteMenu = searchParams.get("showInviteMenu") === "true";
   const member = searchParams.get("member") || "";
-  const { setShowBackDrop, showBackDrop } = useDashboard();
+  const { setShowBackDrop, showBackDrop } = useProject();
   const [emailInput, setEmailInput] = useState(searchParams.get("email") || "");
   const [nameInput, setNameInput] = useState(searchParams.get("name") || "");
+  const role = searchParams.get("role") || "user";
+
   const [msg, setMsg] = useState("");
 
   const invited = searchParams.get("invited") === "true";
@@ -49,6 +53,11 @@ const Members = () => {
   });
   const debouncedEmail = useDebounce(emailInput, 500);
   const debouncedName = useDebounce(nameInput, 500);
+
+  const roleOptions = [
+    { value: "user", label: "User" },
+    { value: "admin", label: "Admin" },
+  ];
 
   useEffect(() => {
     if (showInviteMenu) setShowBackDrop(showInviteMenu);
@@ -81,7 +90,7 @@ const Members = () => {
             withCredentials: true,
           }
         );
-        await fetchOrg();
+        fetchProject();
         handleClose();
       } catch (error) {
         console.log(error);
@@ -149,10 +158,10 @@ const Members = () => {
             onClick={() => handleClose()}
             className="item-3 ml-auto"
           ></button>
-          <h1 className="text-2xl text-center"> Member Info</h1>
+          <h1 className="text-2xl text-center font-bold"> Member Info</h1>
           <h1 className="text-2xl mb-3 text-center">{user.name}</h1>
           <h1 className="text-xl mb-1">{user.email}</h1>
-          {isOrgOwner && userId != user.userId ? (
+          {isOwner && userId != user.userId ? (
             <button
               onClick={() => handleRemoveMember(user.userId)}
               className={`w-full mt-auto ${confirmDelete.id === user.userId && confirmDelete.confirm ? "bg-yellow-500" : "bg-red-500"} text-white p-3 rounded-lg`}
@@ -175,40 +184,69 @@ const Members = () => {
           initial="hidden"
           animate={inView ? "show" : "hidden"}
           ref={ref}
-          className={`opacity-transition ${showBackDrop ? "show" : ""} absolute w-full h-full p-10 z-10 bg-[#e8e8e8] flex flex-col left-0 md:left-1/2 md:top-1/2 transform-center md:h-[450px] md:w-[500px] rounded-xl`}
+          className={`opacity-transition ${showBackDrop ? "show" : ""} absolute w-full h-full p-10 z-10 bg-[#e8e8e8] flex flex-col left-0 md:left-1/2 md:top-1/2 transform-center md:h-[500px] md:w-[500px] rounded-xl`}
         >
           <button
             onClick={() => toggleInviteMenu(false)}
-            className="item-3 ml-auto"
+            className="item-3 ml-auto p-2"
           ></button>
-          <h1 className="text-2xl mb-3 text-center w-full">
-            Invite to {orgName}
+          <h1 className="text-2xl mb-3 text-center w-full font-bold text-primary">
+            Invite to {projectName}
           </h1>
           {invited ? (
             <p className="text-yellow-500 text-center w-full mb-1"> {msg} </p>
           ) : (
             <p className="text-red-500 text-center w-full mb-1"> {msg} </p>
           )}
-          <h1 className="text-xl mb-1">Enter Name</h1>
+          <h1 className="text-xl mb-1 text-primary">Enter Name</h1>
           <input
             type="text"
-            className="w-full h-10 p-6 rounded-xl mb-5"
+            className="w-full h-10 p-6 rounded-xl mb-5 text-primary"
             placeholder="Enter member's name"
             value={nameInput}
             onChange={handleNameChange}
           />
-          <h1 className="text-xl mb-1">Enter Email</h1>
+          <h1 className="text-xl mb-1 text-primary">Enter Email</h1>
           <input
             type="text"
-            className="w-full h-10 p-6 rounded-xl mb-5"
+            className="w-full h-10 p-6 rounded-xl mb-5 text-primary"
             placeholder="Enter member's email"
             value={emailInput}
             onChange={handleEmailChange}
           />
+          <h1 className="text-xl mb-1 text-primary">Select Role</h1>
+          <select
+            className="w-full text-lg p-2 h-10 rounded-xl mb-5 text-primary"
+            value={role}
+            name="role"
+            id="role"
+            onChange={(e) =>
+              setSearchParams(
+                (prev) => {
+                  prev.set("role", e.target.value);
+                  return prev;
+                },
+                { replace: true }
+              )
+            }
+          >
+            <option value="defaultRole" disabled>
+              -- SELECT ROLE --
+            </option>
+            {roleOptions.map((option) => (
+              <option
+                className="text-primary"
+                key={option.value}
+                value={option.value}
+              >
+                {option.label}
+              </option>
+            ))}
+          </select>
 
           <button
             onClick={() => handleInvite()}
-            className="w-full p-3 rounded-xl text-white bg-[#606060]"
+            className="w-full p-3 rounded-xl text-white bg-primary shadow-lg font-bold"
           >
             Invite Member
           </button>
@@ -246,14 +284,15 @@ const Members = () => {
   const handleInvite = async () => {
     if (!validInput()) return;
     try {
-      const response = await axios.post(
-        ADD_MEMBER_ENDPOINT,
-        {
+      const response = await axios.post(ADD_MEMBER_ENDPOINT, {
+        params: {
           email: debouncedEmail,
           name: debouncedName,
+          role: role,
+          projectId: searchParams.get("id"),
         },
-        { withCredentials: true }
-      );
+        withCredentials: true,
+      });
 
       //console.log(response.data);
       setSearchParams((prev) => {
@@ -278,18 +317,18 @@ const Members = () => {
       initial="hidden"
       animate={inView ? "show" : "hidden"}
       ref={ref}
-      className="w-full h-full flex flex-col p-10"
+      className="w-full h-full flex flex-col p-10 font-libreFranklin"
     >
       {renderInviteMenu()}
       {renderMemberInfo()}
-      <h2 className="w-full text-4xl font-ralewayBold text-white p-5 pb-0">
-        {orgName} Members
+      <h2 className="w-full text-4xl font-bold p-5 pb-0">
+        {projectName} Members
       </h2>
-      {isOrgOwner && (
+      {isOwner && (
         <div className="flex p-5 w-full">
           <button
             onClick={() => toggleInviteMenu(true)} // Close invite menu when clicking the button
-            className="p-2 text-2xl flex flex-row gap-2 justify-center items-center rounded-xl w-[230px] bg-[#606060] text-white"
+            className="p-2 text-2xl flex flex-row gap-2 justify-center items-center rounded-xl w-[230px] bg-primary font-bold text-white"
           >
             Invite
           </button>
@@ -302,14 +341,16 @@ const Members = () => {
               <div
                 key={index}
                 onClick={() => toggleMemberInfo(true, member.userId)}
-                className="flex flex-row items-center h-[70px] justify-center hover:cursor-pointer  bg-[#2E2E2E] rounded-xl p-5"
+                className="flex flex-row items-center h-[70px] justify-center hover:cursor-pointer bg-secondary rounded-xl p-5"
               >
-                <p className="text-xl text-white text-center">{member.email}</p>
+                <p className="text-xl text-white font-bold text-center">
+                  {member.email}
+                </p>
               </div>
             );
           })
         ) : (
-          <h2 className="text-2xl font-bold text-white">No Members Found.</h2>
+          <h2 className="text-2xl font-bold">No Members Found.</h2>
         )}
       </div>
     </motion.div>
