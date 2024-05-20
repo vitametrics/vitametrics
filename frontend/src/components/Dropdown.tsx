3ce0f704770626c40c64cdd2f23c1ecf6d0363b3
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-05-19 18:57:08 -0700

    [bugfix] - dropdown corrected route

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from "../helpers/AuthContext";
 
 const Dropdown = () => {
-  const { logout } = useAuth();
+  const { logout, isOwner } = useAuth();
 
   return (
     <div className="absolute top-0 mt-12 mr-5 right-[1rem] bg-white shadow-lg rounded-md overflow-hidden z-50 items-center text-center">
@@ -17,6 +17,14 @@ const Dropdown = () => {
       >
         Settings
       </a>
+      {isOwner && (
+        <a
+          href="/dashboard/admin"
+          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+        >
+          Admin
+        </a>
+      )}
       <a
         onClick={() => logout()}
         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
