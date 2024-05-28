7f7945c5adbe541508291a8135529e12ee9e5034
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-05-27 20:12:36 -0700

    [test] - removed download attribute

--- frontend/src/components/Dashboard/ViewDevice/DeviceDownloadPanel.tsx
+++ frontend/src/components/Dashboard/ViewDevice/DeviceDownloadPanel.tsx
@@ -102,10 +102,7 @@ const DeviceDownloadPanel: React.FC<DeviceDownloadPanelProps> = ({
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute(
-        "download",
-        `device-${deviceId}-${startDate}-${endDate}-${selectedDataTypes.join("-")}.csv`
-      );
+      //link.setAttribute("download", );
       document.body.appendChild(link);
       link.click();
       link.parentNode?.removeChild(link);
