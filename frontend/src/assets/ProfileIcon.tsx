eaabff0c09d6bb2487be1f32519a0ec9976c5a31
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-05-28 03:05:46 -0700

    [chore] - clear error messages in console

--- frontend/src/assets/ProfileIcon.tsx
+++ frontend/src/assets/ProfileIcon.tsx
@@ -10,8 +10,8 @@ const ProfileIcon = () => {
       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
