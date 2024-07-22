3d230bdcb336942cddc682075d7f2341736b63ad
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-07-21 18:50:37 -0700

    [bugfix] - resolved build issue

--- frontend/src/components/DashboardNavbar.tsx
+++ frontend/src/components/DashboardNavbar.tsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.webp";
+import logo from "../assets/images/logo.webp";
 import ProfileIcon from "../assets/ProfileIcon";
 import Dropdown from "./Dropdown";
 import { useState } from "react";
